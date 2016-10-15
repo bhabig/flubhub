765c221d448da7565fd61ed9f01cbfd0a6a0e50c
Bob Habig <rfhab2@gmail.com>
2016-10-15 12:27:00 +0000

    fixed post '/orders' so custom order is created properly

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -24,35 +24,29 @@ class OrdersController < ApplicationController
 
   post '/orders' do
     @user = current_user
-    if !params[:order]
-      item = Item.create(name: params[:item][:name]+" (your custom flurger)", ingredients: params[:ingredients].join(", "), price: 10.99)
-    end
-
     if params[:order]
-      @order = Order.create(params[:order])
-      if !@order.items.empty? && logged_in?
-        @order.order_finished
-        @order.finished_order
-        @order.items << item if item
-        @order.total
-      end
-    else
-      @order = Order.create(user_id: @user.id)
-      #@order.order_completed   #should this be moved to a different controller action?
-      @order.time_started
-      @order.items << item
-      @order.total
+    	@order = Order.create(params[:order])
+    	if params[:ingredients]
+    		@order.items << Item.create(name: params[:item][:name]+" (your custom flurger)", ingredients: params[:ingredients].join(", "), price: 10.99)
+    	end
+    elsif params[:ingredients] && !params[:order]
+    	item = Item.create(name: params[:item][:name]+" (your custom flurger)", ingredients: params[:ingredients].join(", "), price: 10.99)
+    	@order = Order.create(user_id: @user.id)
+    	@order.items << item
     end
+    @order.order_completed
+    @order.time_started
+    @order.total
     @order.save
     @user.orders << @order
-
+    	
     redirect :"/orders/#{@order.id}"
   end
 
   get '/orders/:id' do
     @user = current_user
     @order = Order.find_by_id(params[:id])
-    if @order.user_id == @user.id && @order.order_placed == true
+    if @order.user_id == @user.id && @order.order_completed == true
       erb :'orders/show'
     else
       redirect :"/orders/#{@order.id}/edit"
